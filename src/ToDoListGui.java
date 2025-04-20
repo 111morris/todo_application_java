7ccaff0e3c3ce8e89e81c822cafe25de646594ff
mulandi <mulandimorris1@gmail.com>
2025-04-20 23:04:11 +0300

    added todolist gui class

---
+++ src/ToDoListGui.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+
+public class ToDoListGui extends JFrame {
+  public ToDoListGui(){
+    super("To Do List Application");
+    setDefaultCloseOperation(EXIT_ON_CLOSE);
+  }
+}
