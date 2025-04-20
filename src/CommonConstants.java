49bba34d9f2b9d62a8ef025a95bd72541f121f5f
mulandi <mulandimorris1@gmail.com>
2025-04-20 23:13:40 +0300

    dvpkg

---
+++ src/CommonConstants.java
@@ -0,0 +1,6 @@
+import java.awt.*;
+
+public class CommonConstants {
+  // frame config
+  public static final Dimension GUI_SIZE = new Dimension(540,760);
+}
--- src/ToDoListGui.java
+++ src/ToDoListGui.java
@@ -4,5 +4,17 @@ public class ToDoListGui extends JFrame {
   public ToDoListGui(){
     super("To Do List Application");
     setDefaultCloseOperation(EXIT_ON_CLOSE);
+    setPreferredSize(CommonConstants.GUI_SIZE);
+    pack();
+    setLocationRelativeTo(null);
+    setResizable(false);
+    setLayout(null);
+
+    addGuiComponent();
+  }
+  private void addGuiComponent() {
+    //banner text
+    JLabel bannerLabel = new JLabel("To Do List");
+
   }
 }
