23c4fa046b2ba5d5eae96cc8949bda97260313fa
mulandi <mulandimorris1@gmail.com>
2025-05-01 23:30:17 +0300

    dvpkg

--- src/TaskComponent.java
+++ src/TaskComponent.java
@@ -1,7 +1,6 @@
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
 public class TaskComponent extends JPanel implements ActionListener {
   private JCheckBox checkBox;
   private JTextPane taskField;
