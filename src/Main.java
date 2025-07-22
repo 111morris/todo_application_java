d1b73a379515e0ef947ce3070da95fe98f64cd64
mulandi <mulandimorris1@gmail.com>
2025-07-22 16:15:09 +0300

    dvpkg

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,8 @@ import javax.swing.*;
 public class Main {
   public static void main(String[] args) {
     SwingUtilities.invokeLater(new Runnable() {
-      @Override
-      public void run() {
-        new ToDoListGui().setVisible(true);
-      }
+
+
     });
   }
 }
